63c893aec4183f64d63b8b85fe9f23664b730ecd
Gravity <isaakhern@outlook.com>
2025-11-04 13:51:14 -0500

    Created main python file and lib requirements.txt

---
+++ main.py
@@ -0,0 +1,16 @@
+# FILE: Main.py
+#
+# Description:
+#
+#
+# Test file locations:
+#   - C:\Users\Work\_repos\CompVision\Billard-Train\test\annotated_images
+
+import sys
+import os
+import cv2 as cv
+import torch as tc
+import torchvision as tcv
+import numpy as np  # Mathmatical data structs
+import pandas as pd  # Handle large datasets
+import PIL as pl  # Pillow basic image manipulation
